fdf07cbd897ca7925d99ad0e7e09a1fdcaba3543
dinichthys <den2005425@gmail.com>
2025-03-17 16:17:42 +0300

    Update README

--- main.cpp
+++ main.cpp
@@ -17,13 +17,13 @@
 int main ()
 {
     enum MyPrintfError error = kDonePrintf;
-    ERROR_HANDLER (MyPrintf ("1234567890\n"))
-    int num = 0;
-    ERROR_HANDLER ( MyPrintf ("123\n %n", &num))
-    ERROR_HANDLER ( MyPrintf ("%d\n", num))
-
-    ERROR_HANDLER ( MyPrintf ("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n", -1, -1, "love", 3802, 100, 33, 127,
-                                                                         -1, "love", 3802, 100, 33, 127))
+//     ERROR_HANDLER (MyPrintf ("1234567890\n"))
+//     int num = 0;
+//     ERROR_HANDLER ( MyPrintf ("123\n %n", &num))
+//     ERROR_HANDLER ( MyPrintf ("%d\n", num))
+//
+//     ERROR_HANDLER ( MyPrintf ("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n", -1, -1, "love", 3802, 100, 33, 127,
+//                                                                          -1, "love", 3802, 100, 33, 127))
     float fnum = -123.7;
 
     ERROR_HANDLER ( MyPrintf ("\nMy Printf: \n1: %f\n"
